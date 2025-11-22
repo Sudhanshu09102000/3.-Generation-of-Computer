dbe1747e6eac2238d55bb00a7c7e3a3b44156339
Sudhanshu09102000 <127600086+Sudhanshu09102000@users.noreply.github.com>
2025-11-23 00:20:54 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,391 @@
+const questions = {
+// MCQs start here
+1: {
+text: "Which time period corresponds to the first generation of computers?",
+options: ["1940–1956", "1956–1963", "1964–1971", "1972–2010"],
+correct: "1",
+image: ''
+},
+2: {
+text: "Which hardware component was used in first generation computers?",
+options: ["Vacuum tubes", "Transistors", "Integrated circuits", "VLSI chips"],
+correct: "1",
+image: ''
+},
+3: {
+text: "Which medium was used along with vacuum tubes in first generation computers?",
+options: ["Punch cards", "Keyboards", "Optical disks", "Floppy disks"],
+correct: "1",
+image: ''
+},
+4: {
+text: "Which language did first generation computers support?",
+options: ["Machine language", "Assembly language", "High-level languages", "Object-oriented languages"],
+correct: "1",
+image: ''
+},
+5: {
+text: "How was the performance of first generation computers described?",
+options: ["Slow", "Moderate", "Fast", "Very fast"],
+correct: "1",
+image: ''
+},
+6: {
+text: "Why did first generation computers occupy a large size?",
+options: ["Use of vacuum tubes", "Use of ICs", "Use of microprocessors", "Use of magnetic disks"],
+correct: "1",
+image: ''
+},
+7: {
+text: "How was the storage capacity of first generation computers described?",
+options: ["Poor", "Large", "Unlimited", "Moderate"],
+correct: "1",
+image: ''
+},
+8: {
+text: "What was the electricity consumption characteristic of first generation computers?",
+options: ["Consumed a lot of electricity and generated heat", "Consumed less electricity", "No electricity needed", "Used solar power"],
+correct: "1",
+image: ''
+},
+9: {
+text: "What was the approximate memory size of first generation computers?",
+options: ["4000 bits", "32000 bits", "128000 bits", "100 million bits"],
+correct: "1",
+image: ''
+},
+10: {
+text: "How was data input provided to first generation computers?",
+options: ["Hard-wired programs through punched cards and paper tapes", "Keyboard", "Mouse", "Touch input"],
+correct: "1",
+image: ''
+},
+11: {
+text: "Which of the following is an example of a first generation computer?",
+options: ["ENIAC", "CDC 1604", "IBM 360/370", "Apple II"],
+correct: "1",
+image: ''
+},
+12: {
+text: "Which among the following belongs to first generation computers?",
+options: ["UNIVAC", "Pentium", "IBM 7030", "PARAM"],
+correct: "1",
+image: ''
+},
+13: {
+text: "Which IBM model is an example of a first generation computer?",
+options: ["IBM-701", "IBM 7030", "IBM 360", "IBM Pentium"],
+correct: "1",
+image: ''
+},
+14: {
+text: "Which of the following is also an example of a first generation computer?",
+options: ["IBM-650", "Apple II", "Honeywell 400", "CDC 6600"],
+correct: "1",
+image: ''
+},
+15: {
+text: "Which time period corresponds to the second generation of computers?",
+options: ["1956–1963", "1940–1956", "1964–1971", "1972–2010"],
+correct: "1",
+image: ''
+},
+16: {
+text: "Which component replaced vacuum tubes in second generation computers?",
+options: ["Transistors", "Microprocessors", "VLSI circuits", "LSI chips"],
+correct: "1",
+image: ''
+},
+17: {
+text: "Which storage medium was used in second generation computers?",
+options: ["Magnetic tapes", "Optical disks", "Floppy disks", "SSD drives"],
+correct: "1",
+image: ''
+},
+18: {
+text: "Which operating system type was supported by second generation computers?",
+options: ["Batch operating system", "Time-sharing operating system", "Distributed operating system", "Real-time operating system"],
+correct: "1",
+image: ''
+},
+19: {
+text: "How were second generation computers compared to the first generation?",
+options: ["Faster and smaller", "Slower and bigger", "Same speed", "Unreliable"],
+correct: "1",
+image: ''
+},
+20: {
+text: "How was the reliability and energy efficiency of second generation computers described?",
+options: ["More reliable and energy efficient", "Less reliable", "Same as first generation", "Unstable and energy consuming"],
+correct: "1",
+image: ''
+},
+21: {
+text: "How was the cost of second generation computers compared to the previous generation?",
+options: ["Less costly", "More costly", "Same cost", "Free"],
+correct: "1",
+image: ''
+},
+22: {
+text: "What was the memory capacity of second generation computers?",
+options: ["32,000 bits", "4000 bits", "128,000 bits", "100 million bits"],
+correct: "1",
+image: ''
+},
+23: {
+text: "How was input provided to second generation computers?",
+options: ["Through punched cards", "Through keyboards", "Through mouse", "Through voice input"],
+correct: "1",
+image: ''
+},
+24: {
+text: "Which computer is an example of second generation systems?",
+options: ["Honeywell 400", "IBM 650", "IBM 360", "Pentium"],
+correct: "1",
+image: ''
+},
+25: {
+text: "Which example belongs to second generation computers?",
+options: ["CDC 1604", "CRAY-1", "Apple II", "ENIAC"],
+correct: "1",
+image: ''
+},
+26: {
+text: "Which IBM model is a second generation computer example?",
+options: ["IBM 7030", "IBM-701", "IBM 360", "IBM Pentium"],
+correct: "1",
+image: ''
+},
+27: {
+text: "Which time period corresponds to the third generation of computers?",
+options: ["1964–1971", "1956–1963", "1972–2010", "2010–Present"],
+correct: "1",
+image: ''
+},
+28: {
+text: "Which hardware component was used in third generation computers?",
+options: ["Integrated circuits", "Vacuum tubes", "Microprocessors", "Nano chips"],
+correct: "1",
+image: ''
+},
+29: {
+text: "Which storage media were used in third generation computers?",
+options: ["Large capacity disks and magnetic tapes", "Floppy disks only", "Optical disks", "SSD drives"],
+correct: "1",
+image: ''
+},
+30: {
+text: "Which operating system type was supported by third generation computers?",
+options: ["Time-sharing OS", "Batch OS", "Distributed OS", "Real-time OS"],
+correct: "1",
+image: ''
+},
+31: {
+text: "How were third generation computers described compared to previous generations?",
+options: ["Faster, smaller, more reliable and cheaper", "Slower", "Bigger but faster", "Unreliable"],
+correct: "1",
+image: ''
+},
+32: {
+text: "How was accessibility of third generation computers described?",
+options: ["Easy to access", "Difficult to access", "Restricted access", "Not user-friendly"],
+correct: "1",
+image: ''
+},
+33: {
+text: "What was the memory capacity of third generation computers?",
+options: ["128,000 bits", "32,000 bits", "4000 bits", "100 million bits"],
+correct: "1",
+image: ''
+},
+34: {
+text: "How was input provided to third generation computers?",
+options: ["Keyboards and monitors", "Punch cards only", "Touch screens", "Voice input"],
+correct: "1",
+image: ''
+},
+35: {
+text: "Which of the following is an example of third generation computers?",
+options: ["IBM 360/370", "Apple II", "Pentium", "ENIAC"],
+correct: "1",
+image: ''
+},
+36: {
+text: "Which example belongs to third generation computers?",
+options: ["CDC 6600", "CRAY-X-MP", "IBM 7030", "PARAM"],
+correct: "1",
+image: ''
+},
+37: {
+text: "Which PDP models are examples of third generation computers?",
+options: ["PDP 8/11", "PDP 1", "PDP 15", "PDP 50"],
+correct: "1",
+image: ''
+},
+38: {
+text: "Which time period corresponds to the fourth generation of computers?",
+options: ["1972–2010", "1964–1971", "1956–1963", "2010–Present"],
+correct: "1",
+image: ''
+},
+39: {
+text: "Which technology was used in fourth generation ICs?",
+options: ["VLSI", "Vacuum tubes", "Transistors", "LSI"],
+correct: "1",
+image: ''
+},
+40: {
+text: "Which memory type was used in fourth generation computers?",
+options: ["Semiconductor memory", "Magnetic core memory", "Optical memory", "Cloud memory"],
+correct: "1",
+image: ''
+},
+41: {
+text: "Which input devices were used in fourth generation computers?",
+options: ["Hand held devices, keyboard and mouse", "Punch cards", "Trackball only", "Touch input only"],
+correct: "1",
+image: ''
+},
+42: {
+text: "Which feature was supported by fourth generation computers?",
+options: ["Multiprocessing and distributed OS", "No OS support", "Punch card OS", "Electro-mechanical OS"],
+correct: "1",
+image: ''
+},
+43: {
+text: "Which software type was supported in fourth generation computers?",
+options: ["Object-oriented high level programs", "Only machine language", "Only assembly language", "Binary-only programs"],
+correct: "1",
+image: ''
+},
+44: {
+text: "How were fourth generation computers described in terms of usability?",
+options: ["Small and easy to use", "Large and complex", "Slow and bulky", "Unreliable"],
+correct: "1",
+image: ''
+},
+45: {
+text: "Which cooling requirement applied to fourth generation computers?",
+options: ["No external cooling required", "Heavy cooling required", "Water cooling required", "Liquid nitrogen cooling required"],
+correct: "1",
+image: ''
+},
+46: {
+text: "Which network-related development occurred in the fourth generation?",
+options: ["Development of networks and the internet", "Invention of cloud computing", "Invention of Wi-Fi 6", "Development of 6G"],
+correct: "1",
+image: ''
+},
+47: {
+text: "Which GUI development occurred in fourth generation computers?",
+options: ["New trends in GUIs and mouse", "No GUI existed", "Only command line", "Text-only interfaces"],
+correct: "1",
+image: ''
+},
+48: {
+text: "What was the memory capacity of fourth generation computers?",
+options: ["100 million bits", "128,000 bits", "32,000 bits", "1 billion bits"],
+correct: "1",
+image: ''
+},
+49: {
+text: "Which of the following is an example of fourth generation computers?",
+options: ["Apple II", "ENIAC", "IBM 7030", "PDP-11"],
+correct: "1",
+image: ''
+},
+50: {
+text: "Which VAX model is an example of fourth generation computers?",
+options: ["VAX 9000", "VAX 150", "VAX 200", "VAX 100"],
+correct: "1",
+image: ''
+},
+51: {
+text: "Which CRAY model is a fourth generation supercomputer?",
+options: ["CRAY 1", "CRAY T3E", "CRAY XT5", "CRAY XC40"],
+correct: "1",
+image: ''
+},
+52: {
+text: "Which CRAY system also belongs to fourth generation?",
+options: ["CRAY-X-MP", "CRAY 6600", "CRAY V1000", "CRAY Black"],
+correct: "1",
+image: ''
+},
+53: {
+text: "Which time period corresponds to the fifth generation of computers?",
+options: ["2010–Present", "1972–2010", "1964–1971", "1956–1963"],
+correct: "1",
+image: ''
+},
+54: {
+text: "How are fifth generation computers described?",
+options: ["Modern and advanced", "Mechanical", "Vacuum-based", "Electro-mechanical"],
+correct: "1",
+image: ''
+},
+55: {
+text: "Which hardware is used in fifth generation computers?",
+options: ["ICs with VLSI and Nano technology", "Vacuum tubes", "Magnetic drums", "Relay circuits"],
+correct: "1",
+image: ''
+},
+56: {
+text: "Which storage device supports fifth generation computers?",
+options: ["Large capacity hard disk with RAID", "Floppy disk", "Punch cards", "Paper tape"],
+correct: "1",
+image: ''
+},
+57: {
+text: "Which computational technologies are used in fifth generation computers?",
+options: ["Powerful servers, Internet and cluster computing", "Mechanical switches", "Hydraulic circuits", "Analog systems"],
+correct: "1",
+image: ''
+},
+58: {
+text: "How are fifth generation computers described in terms of usability?",
+options: ["Powerful, cheap, reliable and easy to use", "Slow and costly", "Big and unreliable", "Limited and difficult to use"],
+correct: "1",
+image: ''
+},
+59: {
+text: "Why are fifth generation computers faster and portable?",
+options: ["Use of parallel processors and SLSI circuits", "Use of vacuum tubes", "No processors used", "Only magnetic memory used"],
+correct: "1",
+image: ''
+},
+60: {
+text: "What is the memory capacity of fifth generation computers?",
+options: ["Unlimited", "100 million bits", "128,000 bits", "32,000 bits"],
+correct: "1",
+image: ''
+},
+61: {
+text: "Which input devices are used in fifth generation computers?",
+options: ["CD-ROM, Optical Disk, touch and voice devices", "Punch cards", "Mouse only", "Keyboard only"],
+correct: "1",
+image: ''
+},
+62: {
+text: "Which of the following is an example of fifth generation computers?",
+options: ["IBM", "ENIAC", "CDC 1604", "PDP 8"],
+correct: "1",
+image: ''
+},
+63: {
+text: "Which processor is an example of fifth generation computers?",
+options: ["Pentium", "IBM 650", "CDC 6600", "Apple II"],
+correct: "1",
+image: ''
+},
+64: {
+text: "Which Indian supercomputer is an example of fifth generation?",
+options: ["PARAM", "CRAY 1", "IBM 7030", "Honeywell 400"],
+correct: "1",
+image: ''
+}
+// MCQs end
+
+
+};
+
---
+++ script2.js
@@ -0,0 +1,260 @@
+const explanations = {
+1: {
+text: "The first generation of computers existed from 1940 to 1956. This marks the earliest era of electronic digital computers.",
+image: ""
+},
+2: {
+text: "First generation computers used vacuum tubes as their main hardware component. These tubes performed switching and amplification.",
+image: ""
+},
+3: {
+text: "Punch cards were used in first generation computers along with vacuum tubes to provide input and store data sequences.",
+image: ""
+},
+4: {
+text: "First generation computers supported machine language, the lowest-level programming language consisting of binary instructions.",
+image: ""
+},
+5: {
+text: "The performance of first generation computers was slow due to vacuum tube technology and large hardware delays.",
+image: ""
+},
+6: {
+text: "First generation computers were large in size because they used bulky vacuum tubes which required significant physical space.",
+image: ""
+},
+7: {
+text: "The storage capacity of first generation computers was poor because early memory technologies were extremely limited.",
+image: ""
+},
+8: {
+text: "First generation computers consumed a lot of electricity and generated large amounts of heat due to vacuum tube operations.",
+image: ""
+},
+9: {
+text: "The memory of first generation computers was 4000 bits, which was very small compared to later generations.",
+image: ""
+},
+10: {
+text: "Input in first generation computers was provided through hard-wired programs using punched cards and paper tapes.",
+image: ""
+},
+11: {
+text: "ENIAC is an example of a first generation computer and one of the earliest large-scale electronic digital machines.",
+image: ""
+},
+12: {
+text: "UNIVAC is another example of a first generation computer known for early commercial data processing.",
+image: ""
+},
+13: {
+text: "IBM-701 was one of the first commercial scientific computers and is a first generation model.",
+image: ""
+},
+14: {
+text: "IBM-650 is also a first generation computer widely used in early commercial and scientific work.",
+image: ""
+},
+15: {
+text: "The second generation of computers existed from 1956 to 1963 and replaced vacuum tubes with transistors.",
+image: ""
+},
+16: {
+text: "Transistors were used in second generation computers and provided faster switching and lower power consumption.",
+image: ""
+},
+17: {
+text: "Magnetic tapes were used in second generation computers for data storage and sequential file access.",
+image: ""
+},
+18: {
+text: "Second generation computers supported batch operating systems to process jobs sequentially.",
+image: ""
+},
+19: {
+text: "Second generation computers were faster and smaller than first generation systems due to transistor technology.",
+image: ""
+},
+20: {
+text: "These computers were more reliable and energy efficient compared to the vacuum tube-based first generation.",
+image: ""
+},
+21: {
+text: "Second generation computers were less costly than the first generation due to cheaper transistor components.",
+image: ""
+},
+22: {
+text: "The memory capacity of second generation computers was 32,000 bits, significantly higher than the first generation.",
+image: ""
+},
+23: {
+text: "Input in second generation computers continued to use punched cards for data entry.",
+image: ""
+},
+24: {
+text: "Honeywell 400 is an example of a second generation computer using transistor-based circuits.",
+image: ""
+},
+25: {
+text: "CDC 1604 is a second generation computer known for being one of the earliest fully transistorized systems.",
+image: ""
+},
+26: {
+text: "IBM 7030 is also an example of a second generation machine designed for scientific computing.",
+image: ""
+},
+27: {
+text: "The third generation of computers existed from 1964 to 1971 and used integrated circuits.",
+image: ""
+},
+28: {
+text: "Integrated circuits made from semiconductor materials were the main hardware of third generation computers.",
+image: ""
+},
+29: {
+text: "Large capacity disks and magnetic tapes were used in third generation computers for efficient storage.",
+image: ""
+},
+30: {
+text: "Third generation computers supported time-sharing operating systems, allowing multiple users to work simultaneously.",
+image: ""
+},
+31: {
+text: "These computers were faster, smaller, more reliable and cheaper due to IC technology.",
+image: ""
+},
+32: {
+text: "Third generation computers were easy to access due to improved interfaces like monitors and keyboards.",
+image: ""
+},
+33: {
+text: "The memory capacity of third generation computers was 128,000 bits.",
+image: ""
+},
+34: {
+text: "Input to third generation computers was provided using keyboards and monitors instead of punched cards.",
+image: ""
+},
+35: {
+text: "IBM 360/370 systems are examples of third generation computers using IC-based designs.",
+image: ""
+},
+36: {
+text: "CDC 6600 is a third generation supercomputer known for its high performance.",
+image: ""
+},
+37: {
+text: "PDP 8/11 are examples of third generation minicomputers built using integrated circuits.",
+image: ""
+},
+38: {
+text: "The fourth generation of computers existed from 1972 to 2010 and used VLSI-based ICs.",
+image: ""
+},
+39: {
+text: "Very Large Scale Integration (VLSI) technology was used in fourth generation ICs, enabling thousands of transistors on a chip.",
+image: ""
+},
+40: {
+text: "Semiconductor memory was used in fourth generation computers to provide faster and more compact storage.",
+image: ""
+},
+41: {
+text: "Input to fourth generation computers was provided using hand held devices, keyboards and mouse.",
+image: ""
+},
+42: {
+text: "Fourth generation computers supported multiprocessing and distributed operating systems.",
+image: ""
+},
+43: {
+text: "Object-oriented high level programs were supported in fourth generation computers, improving software development.",
+image: ""
+},
+44: {
+text: "Fourth generation computers were small and easy to use due to microprocessor technology.",
+image: ""
+},
+45: {
+text: "These computers did not require external cooling due to efficient chip design.",
+image: ""
+},
+46: {
+text: "The fourth generation saw the development of networks and the internet.",
+image: ""
+},
+47: {
+text: "This generation developed new trends in graphical user interfaces and mouse input.",
+image: ""
+},
+48: {
+text: "Fourth generation computers had a memory capacity of about 100 million bits.",
+image: ""
+},
+49: {
+text: "Apple II is an example of a fourth generation personal computer.",
+image: ""
+},
+50: {
+text: "VAX 9000 is an example of a fourth generation mainframe computer.",
+image: ""
+},
+51: {
+text: "CRAY 1 is a fourth generation supercomputer known for vector processing.",
+image: ""
+},
+52: {
+text: "CRAY-X-MP is another fourth generation supercomputer that improved multiprocessing.",
+image: ""
+},
+53: {
+text: "The fifth generation of computers exists from 2010 to the present, representing modern systems.",
+image: ""
+},
+54: {
+text: "Fifth generation computers are described as modern and advanced with major improvements.",
+image: ""
+},
+55: {
+text: "These computers use ICs with VLSI and Nano technology for improved performance.",
+image: ""
+},
+56: {
+text: "Fifth generation systems use large capacity hard disks with RAID support to improve storage reliability.",
+image: ""
+},
+57: {
+text: "They use powerful servers, the internet and cluster computing to enhance processing.",
+image: ""
+},
+58: {
+text: "Fifth generation computers are powerful, cheap, reliable and easy to use.",
+image: ""
+},
+59: {
+text: "They are portable and faster due to the use of parallel processors and SLSI circuits.",
+image: ""
+},
+60: {
+text: "The memory capacity of fifth generation computers is considered unlimited due to modern storage expansion.",
+image: ""
+},
+61: {
+text: "Fifth generation computers accept input through CD-ROM, optical disks, touch and voice devices.",
+image: ""
+},
+62: {
+text: "IBM is an example of fifth generation computer technology through its modern systems.",
+image: ""
+},
+63: {
+text: "Pentium processors represent fifth generation computing with advanced microarchitecture.",
+image: ""
+},
+64: {
+text: "PARAM is an Indian fifth generation supercomputer known for high-speed processing.",
+image: ""
+}
+
+
+};
